6718d622163c074b1359a83fa30481a497fb92b6
ramesh <ramesh.c@splenta.com>
2024-09-29 01:38:30 +0530

    initial commit

---
+++ bin/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO FRAUDAPI.GR_USERS (id,created_at,created_by,is_active,updated_at,updated_by,email,mobile_number,password,user_name) VALUES
+	 ('7f000001-8303-12b2-8183-034914600000',NULL,NULL,1,NULL,NULL,NULL,'9000784494','$2a$10$n3ZEz0XgHXNWWsznqUIWI.3QHALKjX60PiMhnMuOCO3EadXIYGJZC','9000784494');
